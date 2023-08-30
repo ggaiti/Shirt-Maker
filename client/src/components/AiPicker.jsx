dc82db6bfceee4d82eac7ba83be0367e166b586b
ggaiti <ggaiti@hotmail.com>
2023-08-29 23:35:57 -0400

    implemented color picker

--- client/src/components/AiPicker.jsx
+++ client/src/components/AiPicker.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const AiPicker = () => {
-  return <div></div>;
+  return <div>ai picker</div>;
 };
 
 export default AiPicker;
--- client/src/components/FilePicker.jsx
+++ client/src/components/FilePicker.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const FilePicker = () => {
-  return <div></div>;
+  return <div>filepicker</div>;
 };
 
 export default FilePicker;
